15e573ca7c337be0580f65fde9ae9dcfbbacfcf6
peerapon1 <peerapon.pen@ngernturbo.com>
2021-08-28 15:07:31 +0700

    inti project

---
+++ src/main/java/demo/BookStore.java
@@ -0,0 +1,11 @@
+package demo;
+
+public class BookStore {
+    public static void main(String[] args) {
+        BookStore bookStore = new BookStore();
+        bookStore.process();
+    }
+    public void process() {
+        // TODO
+    }
+}
